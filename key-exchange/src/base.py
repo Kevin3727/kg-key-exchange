90931401e3424cf79d589f66c9c91336c39f2767
kevin <kevin3727@gmail.com>
2019-06-25 14:48:59 -0400

    added encryption and decryption codes

--- key-exchange/src/base.py
+++ key-exchange/src/base.py
@@ -1,1 +1,38 @@
-impo
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+
+class KeyExchange:
+    def __init__(self):
+        self.__private_key = None
+        self.public_key = None
+
+    def decrypt_message(self, cipher_text):
+        return self.__private_key.decrypt(
+            cipher_text,
+            padding.OAEP(
+                padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+
+    def encrypt_message(self, message):
+        return self.public_key.encrypt(
+            message,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+
+    def generate_private_key(self):
+        self.__private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+            backend=default_backend()
+        )
+        self.public_key = self.__private_key.public_key()

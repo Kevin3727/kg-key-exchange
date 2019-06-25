022bc80d1c83211a0e8c804c80d97ceb2fd454cd
kevin <kevin3727@gmail.com>
2019-06-25 15:01:13 -0400

    added test for inital encryption code

--- key_exchange/test/test_base.py
+++ key_exchange/test/test_base.py
@@ -1,4 +1,17 @@
 from key_exchange.src.base import KeyExchange
 
 
+def test_generate_private_key():
+    ke = KeyExchange()
+    ke.generate_private_key()
+    assert ke.public_key is not None
 
+
+def test_message_encryption():
+    message = b'this is a test message'
+    ke = KeyExchange()
+    ke.generate_private_key()
+    cipher_text = ke.encrypt_message(message)
+    plain_text = ke.decrypt_message(cipher_text)
+    assert message == plain_text, 'plain_text must be equal to the initial ' \
+                                  'message'
